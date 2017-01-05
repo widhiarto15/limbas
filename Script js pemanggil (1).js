bc4d09d163d4064984b7f1d7ef6ee13bc1f08a23
widhiarto15 <masterlimbad00000@gmail.com>
2017-01-05 17:33:55 +0700

    Update Script js pemanggil (1).js

--- Script js pemanggil (1).js
+++ Script js pemanggil (1).js
@@ -21,23 +21,6 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*20)+ 1);var xxx=null;
 if(x=="1"){xxx="/2017/01/android.html"}
 if(x=="2"){xxx="/2017/01/adsense.html"}
-if(x=="3"){xxx="/2017/01/sport-car.html"}
-if(x=="4"){xxx="/2017/01/anak-dan-bunda.html"}
-if(x=="5"){xxx="/2017/01/kesehatan-anak.html"}
-if(x=="6"){xxx="/2015/03/binary-options-explained-simplified.html"}
-if(x=="7"){xxx="/2015/03/10-bank-dengan-aset-terbesar-di.html"}
-if(x=="8"){xxx="/2015/03/best-antivirus-for-android.html"}
-if(x=="9"){xxx="/2015/03/best-smartphones-and-mobile-phones-2015.html"}
-if(x=="10"){xxx="/2015/03/best-new-cars-for-2015.html"}
-if(x=="11"){xxx="/2016/01/10-best-forex-strategies.html"}
-if(x=="12"){xxx="/2016/01/web-hosting-service.html"}
-if(x=="13"){xxx="/2016/01/make-money-in-forex-for-beginners.html"}
-if(x=="14"){xxx="/2016/01/insurance-across-world.html"}
-if(x=="15"){xxx="/2016/01/one-minute-solutions-to-improving-your.html"}
-if(x=="16"){xxx="/2016/01/stress-eating-helps-when-they.html"}
-if(x=="17"){xxx="/2016/01/google-drive-cloud.html"}
-if(x=="18"){xxx="/2016/01/how-to-read-forex-chart-properly.html"}
-if(x=="19"){xxx="/2016/01/auto-insurance-quotes-2015.html"}
 if(x=="20"){xxx="/2016/01/ways-to-get-cheapest-health-insuranc.html"}a_to_ve[i].href="http://jalansafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
