507f4489373ede161ff353861f868e0562bcedea
widhiarto15 <masterlimbad00000@gmail.com>
2017-01-05 17:40:26 +0700

    Update Script js pemanggil (1).js

--- Script js pemanggil (1).js
+++ Script js pemanggil (1).js
@@ -19,8 +19,8 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*20)+ 1);var xxx=null;
-if(x=="1"){xxx="/2017/01/android.html"}
-if(x=="2"){xxx="/2017/01/adsense.html"}a_to_ve[i].href="http://jalansafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="android.html"}
+if(x=="2"){xxx="adsense.html"}a_to_ve[i].href="http://jalansafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
