c670781d7ae84a189e69608849cd83c8530624f4
widhiarto15 <masterlimbad00000@gmail.com>
2017-01-05 23:32:44 +0700

    Update safe-linkini.blogspot.com.js

--- safe-linkini.blogspot.com.js
+++ safe-linkini.blogspot.com.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="sedan.html"}
 if(x=="2"){xxx="ferrari.html"}
 if(x=="3"){xxx="ford.html"}
 if(x=="4"){xxx="mustang.html"}
-if(x=="5"){xxx="bmw.html"}a_to_ve[i].href="https://safe-linkini.blogspot.com/2016/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="bmw.html"}a_to_ve[i].href="https://safe-linkini.blogspot.com/2017/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
