152165cf4c6f52407097e64b768db20d3c2598f6
widhiarto15 <masterlimbad00000@gmail.com>
2017-01-21 14:51:56 +0700

    Update Script js pemanggil versi 2.js

--- Script js pemanggil versi 2.js
+++ Script js pemanggil versi 2.js
@@ -28,7 +28,7 @@ if(x=="6"){xxx="amd.html"}
 if(x=="7"){xxx="adsense.html"}
 if(x=="8"){xxx="blogger.html"}
 if(x=="9"){xxx="forex.html"}
-if(x=="10"){xxx="car-insurance.html"}a_to_ve[i].href="https://geturlsafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="10"){xxx="car-insurance.html"}a_to_ve[i].href="https://geturlsafelink.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
